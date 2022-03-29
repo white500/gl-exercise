707190b86cf80ea6fd8bff550f634a30d457b14d
white500 <964142664@qq.com>
2022-03-29 17:38:34 +0800

    style: 修改注释

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,14 @@
+/*
+ * @Author: yejinliang
+ * @Date: 2022-03-29 14:52:15
+ * @LastEditTime: 2022-03-29 17:07:08
+ * @LastEditors: yejinliang
+ * @Description:
+ */
 /// <reference types="vite/client" />
 
-declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
+declare module "*.vue" {
+  import type { DefineComponent } from "vue"
   // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
   const component: DefineComponent<{}, {}, any>
   export default component
