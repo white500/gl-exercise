addf72888a164ca48da4bef1f1c26c69a24fc3d0
white500 <964142664@qq.com>
2022-03-29 14:57:05 +0800

    删除注释

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
 <!--
  * @Author: yejinliang
  * @Date: 2022-03-23 15:09:05
- * @LastEditTime: 2022-03-28 19:08:45
+ * @LastEditTime: 2022-03-29 14:55:53
  * @LastEditors: yejinliang
  * @Description: 
 -->
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import LightBall from './components/threeComponents/LightBall.vue'
 import CustomBar from './components/echartsComponents/CustomBar.vue'
 import CustomBar1 from './components/echartsComponents/CustomBar1.vue'
